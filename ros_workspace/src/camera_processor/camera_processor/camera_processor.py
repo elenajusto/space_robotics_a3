ce15ad6fb0226900089f2b80deb79053eb47011a
Elena Justo <elena11j@protonmail.ch>
2025-09-24 12:16:44 +1000

    Simplified camera processor's subscription to camera topic. Start converting images to cv2 format and stub for local saving

--- ros_workspace/src/camera_processor/camera_processor/camera_processor.py
+++ ros_workspace/src/camera_processor/camera_processor/camera_processor.py
@@ -1,31 +1,45 @@
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
+# This is a custom node which we are using to listen to the camera
 class CameraProcessor(Node):
 
     def __init__(self):
+        # Node initialisation
         super().__init__('camera_processor')
-        self.subscription = self.create_subscription(
-            Image,
-            '/camera/image',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
-        print("Initialised camera_processor")       # TODO: Debug
 
-    def listener_callback(self, msg):
-        print("Doing the listen...")                # TODO: Debug
-        print("Subscription: ", self.subscription)  # TODO: Debug
-        print("Message: ", msg)                     # TODO: Debug
+         # Initialise CvBridge
+        self.cv_bridge_ = CvBridge()
 
+        # Subscribes to the camera sensor's topic which has messages which contain image
+        self.image_sub_ = self.create_subscription(Image, 'camera/image', self.image_callback, 1)
+
+        # Debug statement
+        print("Initialised camera_processor")       
+
+    # Method within the custom node that is called when data (image messages) from camera topic is received
+    def image_callback(self, msg):
+        
+        # Debug statement
+        print("Doing the listen...")                
+
+        # Convert received message into cv2 format
+        image = self.cv_bridge_.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+
+        # Debug
+        print(image)  # Results: Images are outputted in matrix form
+
+    def image_saver(self, image):
+        # TODO: Handle the process of saving an image to local file
+        
+        pass # TODO: Remove when implemented
 
 def main(args=None):
     rclpy.init(args=args)
 
-    print("Initialising camera_processor...")       # TODO: Debug
+    print("Initialising camera_processor...")     
     
     camera_processor = CameraProcessor()
 
