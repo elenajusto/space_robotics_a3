955407e6a931290922448883c3ff635b43584073
Elena Justo <elena11j@protonmail.ch>
2025-09-24 17:34:06 +1000

    rename files for better organisation

--- ros_workspace/src/camera_processor/camera_processor/camera_processor.py
+++ ros_workspace/src/camera_processor/camera_processor/camera_processor.py
@@ -7,7 +7,7 @@ import time
 import os
 
 # Configure
-local_image_directory = r"capture_images"
+local_image_directory = r"raw_images"
 
 # This is a custom node which we are using to listen to the camera
 class CameraProcessor(Node):
