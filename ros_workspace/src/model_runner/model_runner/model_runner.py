22d46f8214e6945e435edaaf6d1966005e213c9d
Elena Justo <elena11j@protonmail.ch>
2025-10-07 11:12:56 +1100

    init new package to handle running computer vision model

---
+++ ros_workspace/src/model_runner/model_runner/model_runner.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from model_runner.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros_workspace/src/model_runner/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'model_runner'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='elena',
+    maintainer_email='elena11j@protonmail.ch',
+    description='Handle execution of computer vision model and feed into perception system.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'model_runner = model_runner.model_runner:main'
+        ],
+    },
+)
