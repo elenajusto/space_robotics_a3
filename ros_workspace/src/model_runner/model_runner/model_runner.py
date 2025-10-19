a0b67a5a6c4d3d6fc71c51d425a720d3cf72c12e
Elena Justo <elena11j@protonmail.ch>
2025-10-19 16:16:10 +1100

    researching image sizes

--- ros_workspace/src/model_runner/model_runner/model_runner.py
+++ ros_workspace/src/model_runner/model_runner/model_runner.py
@@ -7,6 +7,8 @@ from cv_bridge import CvBridge
 from ultralytics import YOLO
 import cv2
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 class ModelRunnerNode(Node):
     def __init__(self):
@@ -26,7 +28,7 @@ class ModelRunnerNode(Node):
         self.image_sub_ = self.create_subscription(Image, 'camera/image', self.image_callback, 20)
 
     def image_callback(self, image_msg):
-
+         
         # Turn received image into cv format
         image = self.cv_bridge_.imgmsg_to_cv2(image_msg, desired_encoding='passthrough')
         
@@ -44,28 +46,42 @@ class ModelRunnerNode(Node):
             # Boxes object for bounding box outputs
             boxes = result.boxes  
 
+            # TODO - Debug: Image size
+            original_height, original_width = result.orig_shape  
+            self.get_logger().info('image size: %s height by %s width' % (original_height, original_width))
+
+            # Calculate center coordinates
+            center_x = int(original_width / 2)
+            center_y = int(original_height / 2)
+
+            # Draw circle at image center - Assuming this is the principle axis
+            cv2.circle(image, (center_x, center_y), 5, (255, 0, 0), -1) # red circle with radius 5
+            label = "principle axis (Z)"
+            cv2.putText(image, label, (center_x, center_y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
             # Process any detections if they exist
             number_of_boxes = len(boxes.xywh)
             if number_of_boxes > 0:
+
                 # Get name of detected object
                 for box in boxes:
                     class_id = int(box.cls)
                     class_name = self.model.names[class_id]
-                    self.get_logger().info('class_name: "%s"' % class_name)
+                    self.get_logger().info('Detected Object: "%s"' % class_name)
 
                 # Draw bounding boxes and labels
                 for i in range(number_of_boxes):
-                    self.get_logger().info('box: "%s"' % boxes.xywh[i])
+                    self.get_logger().info('Box x, y, width, height: "%s"' % boxes.xywh[i])
 
                     x = int(boxes.xywh[i][0])
                     y = int(boxes.xywh[i][1])
                     width = int(boxes.xywh[i][2])
                     height = int(boxes.xywh[i][3])
 
-                    self.get_logger().info('x: "%s"' % x)
-                    self.get_logger().info('y: "%s"' % y)
-                    self.get_logger().info('width: "%s"' % width)
-                    self.get_logger().info('height: "%s"' % height)
+                    self.get_logger().info('x coordinate of detection on image: "%s"' % x)
+                    self.get_logger().info('y coordinate of detection on image: "%s"' % y)
+                    self.get_logger().info('width of detection box: "%s"' % width)
+                    self.get_logger().info('height of detection box: "%s"' % height)
 
                     # Draw bounding box
                     cv2.rectangle(image, (x, y), (x + height, y + width), (0, 255, 0), 5)
@@ -77,11 +93,18 @@ class ModelRunnerNode(Node):
                     label = f"{class_name} {confidence:.2%}"  # Format confidence as percentage
                     cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
+                    # TODO - Debug: Display image on a plot
+                    plt.imshow(image, interpolation='none') # Plot the image, turn off interpolation
+                    plt.show() # Show the image window
+        
         # Re-convert processed cv image to ros format
         image_detection_message = self.cv_bridge_.cv2_to_imgmsg(image, encoding="rgb8")
 
         # Publish format
         self.image_detections_pub_.publish(image_detection_message)
+
+        # TOOD - Debug: Artificial delay because going to try save some images for investigation
+        time.sleep(10)  # Pause for 10 seconds
 
 def main(args=None):
     rclpy.init(args=args)
